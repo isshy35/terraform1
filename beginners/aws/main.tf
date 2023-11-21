f2783fa5b6ad7aef31c17824e714d562f09a790b
isshy35 <141045132+isshy35@users.noreply.github.com>
2023-11-21 22:40:36 +0100

    Update main.tf

--- beginners/aws/main.tf
+++ beginners/aws/main.tf
@@ -1,9 +1,12 @@
-provider "aws" {
-  version = "~> 5.0"
-  region  = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.0"
+    }
+  }
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+provider "aws" {
+  # Configuration options
 }
